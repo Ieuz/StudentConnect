78cf80d22353b4a00ffbf2bfc30147a0d38c3ce8
Ieuan Harries <2549037h@student.gla.ac.uk>
2022-03-06 17:18:38 +0000

    Added a Django App for the Project, also renamed the project for clarity while coding.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# StudentConnect
+# StudentConnectProject
 WAD Team Project LB01 Team D
---
+++ StudentConnectApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudentconnectappConfig(AppConfig):
+    name = 'StudentConnectApp'
