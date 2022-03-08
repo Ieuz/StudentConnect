4d7995cfb8c72c393fd80af62058bc8fd41cdfed
Ieuan Harries <2549037h@student.gla.ac.uk>
2022-03-08 16:04:45 +0000

    Updated the admin interface so it is visible which choice a question lines up to. Also slightly changed the questions/answers.

--- StudentConnectApp/admin.py
+++ StudentConnectApp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from StudentConnectApp.models import Student, Question, Answer, Choice
 
-# Register your models here.
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question',)
+
 admin.site.register(Student)
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
