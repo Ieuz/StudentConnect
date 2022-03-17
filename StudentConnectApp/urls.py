6740c3150c7599ade4ebdb504209f7718ccc2e3f
Ieuan Harries <ieuanharries2001@gmail.com>
2022-03-17 16:24:51 +0000

    Minor updates fixing the merge

--- StudentConnectApp/urls.py
+++ StudentConnectApp/urls.py
@@ -8,13 +8,10 @@ urlpatterns=[
     path('myAccount/', views.MyAccount, name="myAccount"),
     path('Home/', views.Home, name="Home"),
     path('myMatches/', views.MyMatches, name="myMatches"),
-<<<<<<< HEAD
-=======
     path('Login/', views.Login, name='Login'),
     path('Help/', views.Help, name='Help'),
     path('Signup/', views.Signup, name='Signup'),
     path('Profile/', views.Profile, name='Profile'),
->>>>>>> main
     #add more urls here please
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
--- StudentConnectApp/views.py
+++ StudentConnectApp/views.py
@@ -31,42 +31,6 @@ def Login(request):
     context_dict = {}
     return render(request, 'StudentConnect/login.html', context=context_dict)
 
-<<<<<<< HEAD
-=======
-def Help(request):
-    context_dict = {}
-    return render(request, 'StudentConnect/help.html', context=context_dict)
-
-def Signup(request):
-    context_dict = {}
-    return render(request, 'StudentConnect/signup.html', context=context_dict)
-
-def Profile(request):
-    context_dict = {}
-    return render(request, 'StudentConnect/profile.html', context=context_dict)
-
-
-class ActivateAccount(View):
-
-    def get(self, request, uidb64, token, *args, **kwargs):
-        try:
-            uid = force_text(urlsafe_base64_decode(uidb64))
-            user = User.objects.get(pk=uid)
-        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-            user = None
-
-        if user is not None and account_activation_token.check_token(user, token):
-            user.is_active = True
-            user.profile.email_confirmed = True
-            user.save()
-            login(request, user)
-            messages.success(request, ('Your account have been confirmed.'))
-            return redirect('home')
-        else:
-            messages.warning(request, ('The confirmation link was invalid, possibly because it has already been used.'))
-            return redirect('home')
-
->>>>>>> main
 # register method taken from Tango with Django Chapter 9 - Euan
 
 def register(request):
