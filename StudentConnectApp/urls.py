f54a2420d807df9509f972f219c8e25be76bdbd4
Emma Gort <2577478G@student.gla.ac.uk>
2022-03-22 19:16:01 +0000

    urls added

--- StudentConnectApp/urls.py
+++ StudentConnectApp/urls.py
@@ -8,7 +8,6 @@ urlpatterns=[
     path('myAccount/', views.MyAccount, name="myAccount"),
     path('Home/', views.Home, name="Home"),
     path('myMatches/', views.MyMatches, name="myMatches"),
-    path('findMatches/', views.findMatches, name='findMatches'),
     path('Login/', views.Login, name='Login'),
     path('Help/', views.Help, name='Help'),
     path('Signup/', views.Signup, name='Signup'),
@@ -18,4 +17,5 @@ urlpatterns=[
     path('login/', views.user_login, name='login'),
     path('restricted/', views.restricted, name='restricted'),
     path('logout/', views.user_logout, name='logout'),
+    path('editMyAccount/', views.editMyAccount, name="editMyAccount"),
 ]
