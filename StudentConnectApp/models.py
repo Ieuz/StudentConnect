d307d89856801ad08e9c5afe0429269105fa9de7
Ieuan Harries <ieuanharries2001@gmail.com>
2022-03-24 15:03:29 +0000

    Added drop downs to registration

--- StudentConnectApp/models.py
+++ StudentConnectApp/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+CITY_CHOICES = [
+    ('Aberdeen', 'Aberdeen'),
+    ('Dundee', 'Dundee'),
+    ('Edinburgh', 'Edinburgh'),
+    ('Glasgow', 'Glasgow'),
+    ('Inverness', 'Inverness'),
+    ('Stirling', 'Stirling'),    
+]
+
+SQ_CHOICES = [
+    ("maiden_name", "What is your mother's maiden name?"),
+    ("first_pet", "What is the name of your first pet?"),
+    ("childhood_best_friend", "Who was your childhood best friend?"),
+    ("first_car", "What make was your first car?"),
+    ("first_concert", "What was the first concert you attended?"),
+]
+
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     
     forename = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     date_of_birth = models.DateField()
-    city = models.CharField(max_length=30)
-    security_question = models.CharField(max_length=150)
+    city = models.CharField(max_length=30, choices=CITY_CHOICES)
+    security_question = models.CharField(max_length=150, choices=SQ_CHOICES)
     security_answer = models.CharField(max_length=50)
 
     picture = models.ImageField(null=True, blank=True)    
